ee38907919c5600f36101f51b7c8ca274efbfc0f
fatimaalisoy <fatimaalisoy09@gmail.com>
2024-07-01 11:58:15 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var names = ["YES", "NO", "Definetly","I don't think so", "Probably", "Maybe", "Why Not?", "DEFINETLY YES", "You know the answer", "Why u asking, if u know", "You already know", "Not yet", "Not yet, but soon"]
+var names = ["YES", "NO", "Definetely","I don't think so", "Probably", "Maybe", "Why Not?", "DEFINETELY YES", "You know the answer", "Why u asking, if u know", "You already know", "Not yet", "Not yet, but soon"]
 
 document.getElementById("button").addEventListener("click", ()=>{
     var number =  Math.floor(Math.random() * names.length)
